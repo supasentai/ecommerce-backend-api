cd479dd857896c356eb5d895729e9ccc7f74c1b7
supasentai <dinhluong701@gmail.com>
2026-06-16 17:06:30 +0700

    Implement user registration

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+}
