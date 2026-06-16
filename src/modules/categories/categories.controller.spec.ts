f401483cc135f381628807b530eebca7aa2660e0
supasentai <dinhluong701@gmail.com>
2026-06-16 08:55:49 +0700

    Fix module tests and formatting

--- src/modules/categories/categories.controller.spec.ts
+++ src/modules/categories/categories.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
@@ -7,6 +9,7 @@ describe('CategoriesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
+      providers: [CategoriesService],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
